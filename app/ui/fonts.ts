172652c725d7320386e081a71ee3e6e5f4c13723
橙 曲 <851091009@qq.com>
2024-10-01 18:39:43 +0800

    日产备份

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,10 @@
-import { Inter } from 'next/font/google';
+import {Inter, Lusitana} from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
+export const inter = Inter({subsets: ['latin']});
 
 
+export const lusitana = Lusitana({
+    weight: '400', // 指定字体权重，可以根据实际支持的权重调整
+    subsets: ['latin'], // 指定字符集
+    variable: '--font-lusitana', // 可选变量名称
+});
